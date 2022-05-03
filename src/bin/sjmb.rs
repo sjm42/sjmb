ba06cfdb5c29aea9999c746c12ec01695887fbd1
Sami J. Mäkinen <sjm@iki.fi>
2022-05-03 23:37:47 +0300

    Initial version

---
+++ src/bin/sjmb.rs
@@ -0,0 +1,89 @@
+// irssi-urlharvest.rs
+
+use futures::prelude::*;
+use irc::client::prelude::*;
+use log::*;
+use structopt::StructOpt;
+
+use sjmb::*;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut opts = OptsCommon::from_args();
+    opts.finish()?;
+    start_pgm(&opts, "sjmb");
+    info!("Starting up");
+    let cfg = ConfigCommon::new(&opts)?;
+    debug!("Config:\n{:#?}", &cfg);
+
+    let mut irc = Client::new(opts.irc_config).await?;
+    // trace!("My IRC client is:\n{irc:#?}");
+    irc.identify()?;
+
+    let mut o_nick = String::new();
+    let mut o_user = String::new();
+    let mut o_host = String::new();
+    let mut stream = irc.stream()?;
+    while let Some(message) = stream.next().await.transpose()? {
+        let mynick = irc.current_nickname();
+        trace!("Got msg: {message:?}");
+        if let Some(Prefix::Nickname(nick, user, host)) = message.prefix {
+            o_nick = nick;
+            o_user = user;
+            o_host = host;
+        }
+
+        match message.command {
+            Command::PRIVMSG(channel, text) => {
+                if channel == mynick {
+                    // This is a private msg
+                    info!(
+                        "*Privmsg from {} ({}@{}): {}",
+                        &o_nick, &o_user, &o_host, &text
+                    );
+
+                    if text == cfg.v_password {
+                        info!("Giving voice on {} to {}", &cfg.channel, &o_nick);
+                        if let Err(e) = irc.send_mode(
+                            &cfg.channel,
+                            &[Mode::Plus(ChannelMode::Voice, Some(o_nick.clone()))],
+                        ) {
+                            error!("{e}");
+                        }
+                        let _ = irc.send_privmsg(&o_nick, "You got +v now.");
+                    } else if text == cfg.o_password {
+                        info!("Giving ops on {} to {}", &cfg.channel, &o_nick);
+                        if let Err(e) = irc.send_mode(
+                            &cfg.channel,
+                            &[Mode::Plus(ChannelMode::Oper, Some(o_nick.clone()))],
+                        ) {
+                            error!("{e}");
+                        }
+                        let _ = irc.send_privmsg(&o_nick, "You got +o now.");
+                    } else if o_nick == cfg.owner && text.starts_with("say ") {
+                        let say = &text[4..];
+                        info!("{} <{}> {}", &cfg.channel, mynick, say);
+                        let _ = irc.send_privmsg(&cfg.channel, say);
+                    }
+                } else {
+                    // This is a channel msg
+                    info!("{} <{}> {}", &channel, &o_nick, &text);
+                    if text.contains(mynick) {
+                        let say = "beep boop wat?";
+                        info!("{} <{}> {}", &channel, mynick, say);
+                        let _ = irc.send_privmsg(&channel, say);
+                    }
+                }
+            }
+            Command::Response(resp, v) => {
+                debug!("Got response type {resp:?} contents: {v:?}");
+            }
+            cmd => {
+                debug!("Unhandled command: {cmd:?}")
+            }
+        }
+    }
+    Ok(())
+}
+
+// EOF
---
+++ src/startup.rs
@@ -0,0 +1,72 @@
+// startup.rs
+
+use log::*;
+use serde::{Deserialize, Serialize};
+use std::{env, fs::File, io::BufReader};
+use structopt::StructOpt;
+
+// use super::*;
+
+#[derive(Debug, Clone, StructOpt)]
+pub struct OptsCommon {
+    #[structopt(short, long)]
+    pub verbose: bool,
+    #[structopt(short, long)]
+    pub debug: bool,
+    #[structopt(short, long)]
+    pub trace: bool,
+
+    #[structopt(short, long, default_value = "$HOME/sjmb/config/sjmb.json")]
+    pub bot_config: String,
+    #[structopt(short, long, default_value = "$HOME/sjmb/config/irc.toml")]
+    pub irc_config: String,
+}
+impl OptsCommon {
+    pub fn finish(&mut self) -> anyhow::Result<()> {
+        self.bot_config = shellexpand::full(&self.bot_config)?.into_owned();
+        self.irc_config = shellexpand::full(&self.irc_config)?.into_owned();
+        Ok(())
+    }
+    pub fn get_loglevel(&self) -> LevelFilter {
+        if self.trace {
+            LevelFilter::Trace
+        } else if self.debug {
+            LevelFilter::Debug
+        } else if self.verbose {
+            LevelFilter::Info
+        } else {
+            LevelFilter::Error
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ConfigCommon {
+    pub irc_log_dir: String,
+    pub owner: String,
+    pub channel: String,
+    pub o_password: String,
+    pub v_password: String,
+}
+impl ConfigCommon {
+    pub fn new(opts: &OptsCommon) -> anyhow::Result<Self> {
+        debug!("Reading config file {}", &opts.bot_config);
+        let mut config: ConfigCommon =
+            serde_json::from_reader(BufReader::new(File::open(&opts.bot_config)?))?;
+        config.irc_log_dir = shellexpand::full(&config.irc_log_dir)?.into_owned();
+        Ok(config)
+    }
+}
+
+pub fn start_pgm(c: &OptsCommon, name: &str) {
+    env_logger::Builder::new()
+        .filter_module(name, c.get_loglevel())
+        .format_timestamp_secs()
+        .init();
+    info!("Starting up {name}...");
+    debug!("Git branch: {}", env!("GIT_BRANCH"));
+    debug!("Git commit: {}", env!("GIT_COMMIT"));
+    debug!("Source timestamp: {}", env!("SOURCE_TIMESTAMP"));
+    debug!("Compiler version: {}", env!("RUSTC_VERSION"));
+}
+// EOF
